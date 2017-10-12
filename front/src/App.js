a5b2b08f91f2409a32fd5c226f975a1f6b88a42f
DESKTOP-RO8HTAS\jonit <joni.tammilehto@hotmail.fi>
2017-10-12 13:54:17 +0300

    Test

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
 
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Tikituuba</h1>
+          <h1 className="App-title">Welcome To Tikituuba</h1>
         </header>
 
         <Login />

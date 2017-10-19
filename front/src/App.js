260ac623f9cf18574907f60b38d183c2bf4c542c
Lassi <lassi.hirvonen@pp.inet.fi>
2017-10-19 15:20:51 +0300

    Exo/Exo2 liitetty

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/ui/Login/Login.js';
-import Map from './components/ui/Map/map.js';
+//import Map from './components/ui/Map/map.js';
 import AdminFront from './components/ui/Admin/AdminFront.js';
 // Author: Spagehetti Baker Bros & co.
 
@@ -12,7 +12,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false,
+      loggedIn: true,
 
     }
   }
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
 
         <p>testi</p>
-        {this.state.loggedIn ? <AdminFront /> : <Map />}
+        {this.state.loggedIn ? <AdminFront /> : <Login />}
       </div>
 
 

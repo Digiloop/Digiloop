3a5fede53fe3c5275775cb6557530f61b5f1f2af
Riku Rekola <rerikuju@gmail.com>
2017-11-01 14:34:21 +0200

    new stuff

--- back/index.js
+++ back/index.js
@@ -1,5 +1,23 @@
+var mysql = require('mysql');
 const express = require('express')
 const app = express()
+
+
+var con = mysql.createConnection({
+  host: "niisku.lamk.fi",
+  user: "digiloop",
+  password: "Koodaus1",
+  database: "user_digiloop"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -7,4 +25,18 @@ app.get('/', function (req, res) {
 
 app.listen(4302, function () {
   console.log('Example app listening on port 4302 !')
+})
+
+
+app.get('/nimi', function (req, res){
+
+
+con.query('SELECT * FROM Category', (err,rows) => {
+  if(err) throw err;
+
+  res.send('Data received from Db:\n'+rows);
+});
+
+
+
 })

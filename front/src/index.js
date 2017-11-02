2d6e979e2af2060563c996816e6660c8e66ea0ae
Lassi <lassi.hirvonen@pp.inet.fi>
2017-11-02 10:26:59 +0200

    Redux storen index

--- front/src/index.js
+++ front/src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// debug tools, don't leave in live version
+window.React = React
+window.store = store
+
+
+
+
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,3 +1,21 @@
 import C from '../constants'
 import appReducer from './reducers'
 import { createStore, applyMiddleware } from 'redux'
+
+
+// Entire file is pretty much a test, don't include in prod
+const consoleTest = store => next => action => {
+
+  let result
+
+  console.groupCollapsed(`dispatching action => ${action.type}`)
+  console.log('Logged in', store.getState().loggedIn)
+  result = next(action)
+  console.log("test");
+  console.groupEnd()
+  return result
+}
+
+export default (initialState={}) => {
+  return applyMiddleware(consoleTest)(createStore)(appReducer, initialState)
+}

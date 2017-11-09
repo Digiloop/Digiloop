5da6116e9fedb39523591304a3089caf2b33a1ff
DESKTOP-RO8HTAS\jonit <joni.tammilehto@hotmail.fi>
2017-11-09 13:18:22 +0200

    no message

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
-import Login from './components/ui/Login/Login.js';
 import Map from './components/ui/Map/Map.js';
-=======
 import Login from './components/containers/Login/Login.js';
 //import Map from './components/ui/Map/map.js';
->>>>>>> 3e7591c05f35947541b8549b16b5838ec8c9ae74
 import AdminFront from './components/ui/Admin/AdminFront.js';
 // Author: Spagehetti Baker Bros & co.
 
@@ -19,7 +15,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loggedIn: false,
+      loggedIn: true,
 
     }
   }

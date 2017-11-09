4a1638712ac0b988051720937a8acab4f8723b70
Riku Rekola <rerikuju@gmail.com>
2017-11-09 13:16:28 +0200

    expressia

--- back/express/routes/users.js
+++ back/express/routes/users.js
@@ -1,5 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var testi;
+var testi2;
+
+var con = mysql.createConnection({
+  host: "niisku.lamk.fi",
+  user: "digiloop",
+  password: "Koodaus1",
+  database: "user_digiloop"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+	con.query('SELECT * FROM Category', (err, rows) => {
+        testi = rows[0].CatName;
+	testi2 = rows[1].CatName;
+	if (err) throw err;
+    });
+  
+  console.log("Connected!");
+  
+    
+});
+
+console.log(testi);
+
+
+
+
+
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,10 +42,10 @@ router.get('/', function(req, res, next) {
   // And insert something like this instead:
   res.json([{
   	id: 1,
-  	username: "samsepi0l"
+  	username: testi
   }, {
   	id: 2,
-  	username: "D0loresH4ze"
+  	username: testi2
   }]);
 });
 

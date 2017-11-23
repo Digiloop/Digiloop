d2c8035876c253ea0e55d96647fa1b45e10ed51c
DigiLoop-projekti <digiloop.lamk@gmail.com>
2017-11-23 10:10:11 +0200

    readded files

---
+++ back/express/config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'niisku.lamk.fi',
+        'user': 'digiloop',
+        'password': 'Koodaus1'
+    },
+	'database': 'user_digiloop',
+    'users_table': 'users',
+    'Category_table': 'Category'
+};
---
+++ back/express/server.js
@@ -0,0 +1,52 @@
+// server.js
+
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app      = express();
+var port     = process.env.PORT || 4334;
+
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+var users;
+var source = require('./config/users.js');
+// configuration ===============================================================
+// connect to our database
+//console.log('server' + source.Testi1);
+
+require('./config/passport')(passport); // pass passport for configuration
+//require('./config/users')(users);
+
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+	secret: 'helloworldisthreehundredfiftysixbilliontree',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport, users); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);

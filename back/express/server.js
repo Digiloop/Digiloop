7114bdfb79b529bd93212d00604d5b7a737ed63f
rekoriku <rerikuju@gmail.com>
2017-12-12 11:18:23 +0200

    -

--- back/express/server.js
+++ back/express/server.js
@@ -31,7 +31,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating not actually used in this project
 
 // required for passport
 app.use(session({

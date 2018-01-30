989769a593a39ba88f340b0c981b6ec1357e8f2f
Lassi <lassi.hirvonen@pp.inet.fi>
2018-01-30 11:36:21 +0200

    fletcherin uusiotyön aloitus

--- front/src/store/reducers.js
+++ front/src/store/reducers.js
@@ -8,11 +8,20 @@ export const loggedIn = (state=[], action) =>
 export const resListOpt = (state=[], action) =>
   (action.type === C.SET_RESOPT) ? action.payload : state
 
+/*
 export const resList = (state=[], action) =>
   (action.type === C.SET_RLI) ? action.payload : state
+*/
+
+
+
+
+
+
+
 
 export default combineReducers({
   loggedIn,
-  resListOpt,
-  resList
+  resListOpt
+  //resList
 })

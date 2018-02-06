891a4cfa5b1e42d7b9a997c1acf4e301496ddee5
Jaakko Mutka <jaakko.mutka1@student.lamk.fi>
2018-02-06 12:56:47 +0200

    moi #6

--- back/express/app/routes.js
+++ back/express/app/routes.js
@@ -1,6 +1,11 @@
 // app/routes.js
 //var catquery = require('../config/catquery');
 var source = require('../config/users.js');
+// these are copypasted from users.js DO NOT DELETE :D
+var express = require('express');
+var router = express.Router();// load up the user model
+var mysql = require('mysql2');
+var dbconfig = require('./database');
 var connection = mysql.createConnection(dbconfig.connection);
 
 module.exports = function(app, passport, users) {

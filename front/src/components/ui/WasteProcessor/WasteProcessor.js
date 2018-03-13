245a4c2ad7fd151e536db9b9e646df38800f415a
Lassi <lassi.hirvonen@pp.inet.fi>
2018-03-13 13:08:13 +0200

    css korjattu'd

--- front/src/components/ui/WasteProcessor/WasteProcessor.js
+++ front/src/components/ui/WasteProcessor/WasteProcessor.js
@@ -22,12 +22,11 @@ constructor(props){
   }
  }
 
-  handleChange = (value) => {
-
-    this.setState({
-       index: value
-    });
-  };
+handleChange = (value) => {
+  this.setState({
+    index: value
+  });
+};
 
 render() {
 
@@ -35,12 +34,12 @@ render() {
     return (
       <MuiThemeProvider>
       <div>
-      <Tabs index={this.state.index} onChange={this.handleChange}>
-        <Tab label="Historia" value={0} />
-        <Tab label="Varaukset" value={1} />
-        <Tab label="Admin" value={2} />
-        <Tab label="Varauskartta" value={3} />
-        <Tab label="Ilmoitukset" value={4} />
+      <Tabs index={this.state.index} onChange={this.handleChange} inkBarStyle={{background: '#AFD43F', height: '3px'}}>
+        <Tab label="Historia" className="menu" value={0} />
+        <Tab label="Varaukset" className="menu" value={1} />
+        <Tab label="Admin" className="menu" value={2} />
+        <Tab label="Varauskartta" className="menu" value={3} />
+        <Tab label="Ilmoitukset" className="menu" value={4} />
       </Tabs>
       {this.state.index === 0 && <HistoryListing />}
       {this.state.index === 1 && <ReservedListing />}

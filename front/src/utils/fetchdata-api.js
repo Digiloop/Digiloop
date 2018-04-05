e549c2f5da5cb782843727f819f4674f5fe356f3
Erno <erno.viitanen1@student.lamk.fi>
2018-04-05 11:53:06 +0300

    items2

--- front/src/utils/fetchdata-api.js
+++ front/src/utils/fetchdata-api.js
@@ -5,5 +5,5 @@ import { BASE_URL } from '../settings';
 export {getJunkData};
 
 function getJunkData() {
-  return axios.get(BASE_URL+'/items').then(response => response.data);
+  return axios.get(BASE_URL+'/items2').then(response => response.data);
 }

ed9ebbccdea15424afd1904e73d1ded22d714021
rekoriku <rerikuju@gmail.com>
2018-05-04 14:45:34 +0300

    subcatstatus post added

--- back/express/routes/categories.js
+++ back/express/routes/categories.js
@@ -5,60 +5,29 @@ var router = express.Router();
 
 //https://javascript.info/async-await
 //https://itnext.io/using-async-await-to-write-cleaner-route-handlers-7fc1d91b220b
-
-/*
+//GET
 router.get('/categories', function(req, res, next) {
-        connection.query('SELECT * FROM Category WHERE Status = 1', function(err, result) {
-            if (err) throw err;
-            res.json({
-                category: result
-            });
-        });
-    });
-*/
-/*(taulu = 'Category', tieto = 'Status')*/
+  dosmth.getinfo('Category','1',function(err, result){
+    if (err) throw err;
+    res.json(result);
+    next();
+  });
+});
 
-    router.get('/categories7',/*dosmth.getcategories,*/ async function(req, res, next) {
+router.get('/subcat', function(req, res, next) {
+  dosmth.getinfo('subCat','1',function(err, result){
+    if (err) throw err;
+    res.json(result);
+    next();
+  });
+});
 
-          //await res.json(dosmth.getcategories('Category','1'));
-          //console.log(dosmth.getcategories('Category','1'));
-          //res.json(await dosmth.getcategories('Category','1'))
-          //await console.log(dosmth.getcategories('Category','1'));
-          /*res.json(dosmth.getcategories(taulu = 'Category', tieto = 'Status'))*/
-        //  throw new Error("Whoops!");
-    //  next();
-        });
-
-
-        router.get('/categories',/*dosmth.getcategories,*/ async function(req, res, next) {
-              //res.json(await dosmth.getinfo('Category','1'));
-              await dosmth.getinfo('Category','1',function(err, result){
-                res.json(result);
-              });
-            });
-
-
-/*
-        router.get('/categories',dosmthelse.getcategoriestemp, async function(req, res, next) {
-
-            });
-
-*/
-
-
-
-
-
-    router.get('/error', function(req, res, next) {
-            connection.query('SELECT * FROM Category WHERE Status = 1', function(err, result) {
-                if (err) throw err;
-                res.json({
-                    category: ServuKaatuu
-                });
-            });
-        });
-
-
+//POST
+router.post('/subcatstatus', function(req, res, next) {
+  dosmth.updatesubcatstatus('subCat',req.body.Status,req.body.subIdStatus)
+    console.log("Status ",req.body.Status," subIdStatus ", req.body.subIdStatus)
+    res.end();
+});
 
 
 module.exports = router

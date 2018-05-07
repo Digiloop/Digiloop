1e1075dec916dc0b7699e79e848ac5302e431e01
Lassi <lassi.hirvonen@pp.inet.fi>
2018-05-07 12:35:10 +0300

    varausluettelon avautuvuus edistyy

--- front/src/components/ui/WasteProcessor/Varauskartta/ReservationListing.js
+++ front/src/components/ui/WasteProcessor/Varauskartta/ReservationListing.js
@@ -51,6 +51,11 @@ class ReservationListing extends Component {
   expand(x) {
     console.log(x);
     console.log(this.state.rows);
+
+    let newArray = this.state.rows;
+    newArray[x] = !newArray[x];
+
+    this.setState({ rows: newArray });
   }
 
   // create states for each row
@@ -64,15 +69,16 @@ class ReservationListing extends Component {
   }
 
 
-  componentWillReceiveProps(){
+  componentWillReceiveProps(nextProps){
     let arr = this.state.rows;
     console.log(this.props.items);
     console.log(this.props);
+    console.log(nextProps);
 
-    for (let i = 0; i < this.props.items.length; i++) {
-      arr: [...arr, false]
+    for (let i = 0; i < nextProps.items.length; i++) {
+      arr = [...arr, false]
     }
-    this.setState({ rows: arr }, console.log(this.state.rows));
+    this.setState({ rows: arr });
   }
 
   render() {
@@ -87,9 +93,12 @@ class ReservationListing extends Component {
       items.push(
         <TableRow key={i} >
           <TableRowColumn>{this.props.items[i].category} ({this.props.items[i].subCat})<br />Ilmoitettu: {this.props.items[i].date}</TableRowColumn>
-          <TableRowColumn>{this.props.items[i].pcs}kpl</TableRowColumn>
+          
+          {this.state.rows[i] ? <TableRowColumn>{this.props.items[i].pcs}kpl</TableRowColumn>: <TableRowColumn></TableRowColumn>}
           <TableRowColumn>{this.props.items[i].size}m<sup>3</sup></TableRowColumn>
           <TableRowColumn>{this.props.items[i].weight}kg</TableRowColumn>
+          
+
           {this.props.items[i].status == 1 ? <TableRowColumn><RaisedButton label="Varaa" onClick={e => this.reserve(this.props.items[i])} /></TableRowColumn> : <TableRowColumn></TableRowColumn>}
           <TableRowColumn>Tila {this.getStatus(this.props.items[i].status)}</TableRowColumn>
         </TableRow>

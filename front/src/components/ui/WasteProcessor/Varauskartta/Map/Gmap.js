e255a78f5706a327ea34ac91b8b28fac29db40e8
Lassi <lassi.hirvonen@pp.inet.fi>
2018-05-18 15:55:00 +0300

    Location filter & map fixes

--- front/src/components/ui/WasteProcessor/Varauskartta/Map/Gmap.js
+++ front/src/components/ui/WasteProcessor/Varauskartta/Map/Gmap.js
@@ -6,133 +6,102 @@ import TextField from 'material-ui/TextField';
 
 import { compose, withProps, withStateHandlers } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
-import  MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel"
+import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel"
 import PropTypes from 'prop-types'
 import InfoBox from "react-google-maps/lib/components/addons/InfoBox"
 
 class Map extends Component {
-componentWillMount(){
-  this.setState({
-      test:[]
+  componentWillMount() {
+    this.setState({
+      test: []
     })
-}
+  }
 
-constructor(props){
-  super(props);
+  constructor(props) {
+    super(props);
 
-  this.state={
+    this.state = {
       visible: false,
       center: {
-          lat: 33.6890603,
-          lng: -78.8866943
-          },
-          true: true,
-          markers: [],
-          address: ["Kouvola", "Lahti", "Berlin"],
-          rows: [],
-          needClearing: true,
-          test: ["Kikki", "Hiiri"]
+        lat: 33.6890603,
+        lng: -78.8866943
+      },
+      true: true,
+      markers: [],
+      address: ["Kouvola", "Lahti", "Berlin"],
+      rows: [],
+      needClearing: true,
+      test: ["Kikki", "Hiiri"]
+
+    }
+  }
+
+
+
+  clear() {
+    //console.log(this.state.cleared);
+    if (this.state.needClearing) {
+      this.setState({
+        rows: [],
+        needClearing: false,
+        test: ["Dolan"]
+      });
+      //console.log("Cleared!");
+    }
+    else {
+      this.setState({
+        needClearing: true
+      });
+    }
+  }
+
+  clearTEST() {
+    this.setState({
+      test: []
+    });
 
   }
- }
-
-/*componentDidUpdate(prevProps, prevState){
-
-if(this.state.test !== prevState.test){
-this.setState({
-    test:[]
-
-  })
-}
-}*/
 
 
-clear(){
-  //console.log(this.state.cleared);
-  if(this.state.needClearing){
-  this.setState({
-    rows: [],
-    needClearing: false,
-    test:["Dolan"]
-  });
-  //console.log("Cleared!");
-}
-else{
-  this.setState({
-    needClearing:true
-  });
-}
-}
-
-clearTEST(){
-  this.setState({
-    test: []
-  });
-  //console.log("Cleared!TEST");
-
-}
-
-/*pressed(){
-  if (this.visible) {
-    this.visible=false;
-  }
-  else{
-    this.visible=true;
-  }
-  var markers = [{"lat": 60.986466, "lng": 25.643688, "text": "Kikki Hiiri was here!"},
-  {"lat": 61.986466, "lng": 26.643688, "text": "Kikki Hiiri was here too!"}];
-  console.log("Hiiohoi");
-  console.log(markers[0].text);
-}*/
-
-/*
- debug(){
-   //console.log("Hiiohoi");
- }*/
-
-render() {
-  //console.log(this.state.rows[0]);
-  //console.log(this.state.test[0]);
-  //console.log(this.props.items);
-    for ( let i = 0; i < this.props.items.length; i++ ){
-        this.state.rows[i] = <Marker
-          key= {i}
-          position={{ lat: parseInt(this.props.items[i].latitude), lng: parseInt(this.props.items[i].longitude) }} />
-          //console.log(this.props.items[i]);
-          //console.log("Marker added");
-      }
+  render() {
+    for (let i = 0; i < this.props.items.length; i++) {
+      console.log("Lat: " + this.props.items[i].latitude + " Long: " + this.props.items[i].longitude)
+      this.state.rows[i] = <Marker
+        key={i}
+        position={{ lat: parseFloat(this.props.items[i].latitude), lng: parseFloat(this.props.items[i].longitude) }} />
+    }
 
 
-  const MapComp = compose(
-   withProps({
-     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAN0SNyI8W4PSk8x6kbS0XRcXGLODokzBw",
-     loadingElement: <div style={{ height: `100%` }} />,
-     containerElement: <div style={{ height: `400px` }} />,
-     mapElement: <div style={{ height: `100%` }} />,
-     visible: false,
-     center: {  lat: 60.986466, lng: 25.643688 },
-   }),
-   withStateHandlers(() => ({
-      isOpen: false,
-    }), {
-      onToggleOpen: ({ isOpen }) => () => ({
-        isOpen: !isOpen,
-      })
-    }),
-   withScriptjs,
-   withGoogleMap
-  )((props) =>
-   <GoogleMap
-     defaultZoom={8}
-     defaultCenter={{ lat: 60.986466, lng: 25.643688 }}
-   >
-  {this.state.rows}
-   </GoogleMap>
-  );
+    const MapComp = compose(
+      withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAN0SNyI8W4PSk8x6kbS0XRcXGLODokzBw",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+        visible: false,
+        center: { lat: 60.986466, lng: 25.643688 },
+      }),
+      withStateHandlers(() => ({
+        isOpen: false,
+      }), {
+          onToggleOpen: ({ isOpen }) => () => ({
+            isOpen: !isOpen,
+          })
+        }),
+      withScriptjs,
+      withGoogleMap
+    )
+    ((props) =>
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: 60.986466, lng: 25.643688 }}>
+        {this.state.rows}
+      </GoogleMap>
+    );
 
     return (
       <div>
-          <MapComp />
+        <MapComp />
 
       </div>
     );

3031248cb4e4f13405ef8a777382fe94aa5051f8
Lassi <lassi.hirvonen@pp.inet.fi>
2018-06-08 16:30:39 +0300

    Filtterit ja korjaukset adminin käyttäjälistaukseen

--- front/src/components/ui/Admin/Admin.js
+++ front/src/components/ui/Admin/Admin.js
@@ -5,12 +5,18 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { getUsers } from '../../../utils/fetchUsers';
 import { TableRow, TableRowColumn, Table, TableBody } from 'material-ui';
 
+import Checkbox from 'material-ui/Checkbox';
+
 class Admin extends Component {
   constructor(props) {
     super(props);
     this.state = {
       users: [],
-      rows: []
+      rows: [],
+
+      listAdmins: true,
+      listWasteprocessors: true,
+      listEndUsers: true
     }
   }
 
@@ -22,7 +28,14 @@ class Admin extends Component {
   }
 
   // opening items
-  expand(x) {
+  expand(x, visibleRowsCount) {
+
+    console.log("Sepon spagetit")
+    console.log(x)
+    console.log(visibleRowsCount)
+    console.log(this.state.rows)
+
+    /*
     // create a temp array, because it's easier to edit than the state one
     let newArray = this.state.rows;
 
@@ -36,6 +49,22 @@ class Admin extends Component {
       }
       newArray[x] = true; // open the new row
     }
+    */
+
+    let newArray = [];
+
+    for(let i = 0; i < visibleRowsCount; i++){
+      if(i === x){
+        if(this.state.rows[x] == true){
+          newArray[i] = false;
+        } else {
+          newArray[i] = true;
+        }
+      } else {
+        newArray[i] = false;
+      }
+    }
+
     // set the edited version as the new state
     this.setState({ rows: newArray });
   }
@@ -47,47 +76,69 @@ class Admin extends Component {
   render() {
 
     const users = [];
-    console.log(this.state.users);
+    let visibleRowsCount = 0;
 
     // get users
     for (let i = 0; i < this.state.users.length; i++) {
-      if (this.state.rows[i]) {
-        users.push(
-          <TableRow key={i} style={{ height: '200px', backgroundColor: '#CCC' }}>
-            <TableRowColumn colSpan='1'>
-              Nimi:<br />
-              Osoite:<br />
-              Kaupunki:<br />
-              Sähköposti:<br />
-              Puhelinnumero:<br />
-            </TableRowColumn>
-            <TableRowColumn colSpan='2'>
-              {this.state.users[i].fname}{' ' + this.state.users[i].lname}<br />
-              {this.state.users[i].address}<br />
-              {this.state.users[i].zipcode}{' '+this.state.users[i].city}<br />
-              {this.state.users[i].email}<br />
-              {this.state.users[i].phone}
-            </TableRowColumn>
-            <TableRowColumn colSpan='1'>
-              Button tulee tähä
-            </TableRowColumn>
-          </TableRow>
-        )
-      } else {
-        users.push(
-          <TableRow key={i}>
-            <TableRowColumn colSpan='4'>
-              {this.state.users[i].fname}{' ' + this.state.users[i].lname}
-            </TableRowColumn>
-          </TableRow>
-        )
+
+      if ((this.state.users[i].userlvl == 0 && this.state.listAdmins)
+        || (this.state.users[i].userlvl == 1 && this.state.listWasteprocessors)
+        || (this.state.users[i].userlvl == 2 && this.state.listEndUsers)
+      ) {
+        if (this.state.rows[visibleRowsCount]) {
+          users.push(
+            <TableRow key={i} style={{ height: '200px', backgroundColor: '#CCC' }}>
+              <TableRowColumn colSpan='1'>
+                Nimi:<br />
+                Osoite:<br />
+                Kaupunki:<br />
+                Sähköposti:<br />
+                Puhelinnumero:<br />
+              </TableRowColumn>
+              <TableRowColumn colSpan='2'>
+                {this.state.users[i].fname}{' ' + this.state.users[i].lname}<br />
+                {this.state.users[i].address}<br />
+                {this.state.users[i].zipcode}{' ' + this.state.users[i].city}<br />
+                {this.state.users[i].email}<br />
+                {this.state.users[i].phone}
+              </TableRowColumn>
+              <TableRowColumn colSpan='1'>
+                Button tulee tähä
+              </TableRowColumn>
+            </TableRow>
+          )
+        } else {
+          users.push(
+            <TableRow key={i}>
+              <TableRowColumn colSpan='4'>
+                {this.state.users[i].fname}{' ' + this.state.users[i].lname}
+              </TableRowColumn>
+            </TableRow>
+          )
+        }
+        visibleRowsCount++;
       }
+
     }
 
 
     return (
       <MuiThemeProvider>
-        <Table style={{ width: '60%', marginLeft: '5%', marginTop: '4%' }} onCellClick={rowNumber => this.expand(rowNumber)}>
+
+        <Checkbox
+          checked={this.state.listAdmins}
+          onCheck={(event, newValue) => this.setState({ listAdmins: newValue })}
+        /> Sepot
+        <Checkbox
+          checked={this.state.listWasteprocessors}
+          onCheck={(event, newValue) => this.setState({ listWasteprocessors: newValue })}
+        /> Tepot
+        <Checkbox
+          checked={this.state.listEndUsers}
+          onCheck={(event, newValue) => this.setState({ listEndUsers: newValue })}
+        /> Jepet
+
+        <Table style={{ width: '60%', marginLeft: '5%', marginTop: '4%' }} onCellClick={rowNumber => this.expand(rowNumber, visibleRowsCount)}>
           <TableBody displayRowCheckbox={false} >
             {users}
           </TableBody>

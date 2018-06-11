d5c44e3e4c40411c483f740d8cf928ccff98a9f7
Arcturus mengsk <arttu.nuolioja@gmail.com>
2018-06-11 16:28:09 +0300

    Korjattu kuvan esikatselu tepolle

--- front/src/components/ui/WasteProcessor/Varauskartta/ReservationListing.js
+++ front/src/components/ui/WasteProcessor/Varauskartta/ReservationListing.js
@@ -125,7 +125,7 @@ class ReservationListing extends Component {
             {this.props.items[j].size}m<sup>3</sup><br />
             {this.props.items[j].weight}kg<br />
 
-            <img src={imageUrl} />
+            <img src={imageUrl} style={{maxWidth: '200px'}}  />
 
             <div>{this.props.items[j].description}</div><br />
             </TableRowColumn>

8a5758db44ac529f460fb83d4d6454474847c520
rekoriku <rerikuju@gmail.com>
2018-06-13 10:10:20 +0300

    temporary changes

--- back/express/routes/items.js
+++ back/express/routes/items.js
@@ -14,18 +14,19 @@ router.get('/items', (req, res, next) => {
 });
 
 //POST
+/*
 router.post('/imageAdd', (req, res) => {
     //console.log(req.get('Content-Type'));
     console.log(req.files)
     //console.log(req.files.pic)
     //console.log(typeof req.files.pic)
     console.log(req.body)
-    /*
+    
       const query = 'UPDATE junk' +  SET  + cat[2] + ' = ? WHERE ' + cat[1] + '  = ?'
       imgname = `${req.body.id}_${req.files.pic.name}`;
       misk.imageAdd(req.files.pic, req.body.catType, imgname)
       sqldatahaku.querySql(query, [imgname, req.body.id])
-    */
+    
     //console.log(typeof req.files.image);
     //console.log(req.body.image);
     //console.log(req.body);
@@ -36,31 +37,46 @@ router.post('/imageAdd', (req, res) => {
     res.end();
     //console.log(req.files.sample);
 });
+*/
 
-
-router.post('/itemAdd', (req, res, next) => {
-    //console.log(req.body.itemData)
-    //console.log(req.body)
-    //console.log(req.body.pickupInstructions0)
-    //console.log(req.files)
-    console.log(req.body)
-    //console.log(typeof req.body.itemData)
-    //console.log(misk.checkValidLength(req.body.itemData, 14))
-    //console.log(misk.checkValidValues(req.body.itemData));
-    //misk.loopityLoop(req.body.itemData,console.log)
-    //console.log(req.body.itemData)
-    //console.log(Object.keys(req.body.itemData[0].picture).length)
-    //console.log(misk.imageAdd(req.body.itemData[0].req.files.picture, 0));
-    //if(req.files == null){third = 'picture'}else{
+router.post('/itemAddOld', (req, res, next) => {
     
-    let third = misk.keyToArray(req.files)
-    //}
-   console.log(third)
+    //console.log(req.files)
+    console.log(third)
     const query = 'INSERT INTO junk ( category, city, description, iscompany, latitude, longitude, pcs, itemphone, wishbox, pickupaddr,  size, subCat, weight, zipcode, junkdate, junkdateadded, status, owner, picture ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);'
-    const secondary = [Date.now(), misk.dateThing(), 1, req.user.id]//req.user.id
+    const secondary = [Date.now(), misk.dateToday(), 1, req.user.id]//req.user.id
     //let imuguu = misk.keyToArray(req.files)[1].name
     // console.log(imuguu)
-    misk.fuseItemArray(req.body, secondary, third, sqldatahaku.querySql, query)//req.body = all items, secondary = data from backend, sqldatahaku.querySql = function that inserts stuff to database, query = sqlquery
+    //misk.fuseItemArray(req.body, secondary, third, sqldatahaku.querySql, query)//req.body = all items, secondary = data from backend, sqldatahaku.querySql = function that inserts stuff to database, query = sqlquery
+    res.end();
+});
+
+router.post('/itemAdd', (req, res, next) => {
+    a = req.body
+    //console.log(a)
+    //console.log(a)
+    var arr = Object.entries(a);
+
+    //for (i = 0; i < a; i++) {}
+    let pusheri = []
+    
+    arr.forEach(([key, value]) => {
+        if (key.includes('img')){
+            key.pop(key,value)
+        }
+        //console.log(`${key} ${value}`); 
+        });
+
+        console.log(arr)
+/*
+//console.log(pusheri)
+    for (var key in a) {
+        //arr.push(vals[key]);
+        if (key.includes('img')){
+            //arr.push(key)
+        }
+    }
+*/
     res.end();
 });
 
@@ -69,20 +85,26 @@ router.post('/itemStatus', (req, res, next) => {
     res.end();
 })
 
+router.post('/itemReserveCancel', (req, res, next) => {
+    sqldatahaku.querySql('UPDATE junk SET status = ?,fetcher = ? WHERE junkID = ?;', [1, 0, req.body.junkId])
+    res.end();
+})
+
+//tarttee checking että ei voi varata muitten varattuja
 router.post('/itemReserve', (req, res, next) => {
     sqldatahaku.querySql('UPDATE junk SET status = ?,fetcher = ? WHERE junkID = ?;', [2, req.user.id, req.body.junkId])
     res.end();
 })
 
 router.get('/stuff', (req, res, next) => {
-    res.json(misk.dateThing())
+    res.json(misk.dateToday())
 })
 
 //var myDate = new Date();
 //myDate.setHours(myDate.getHours() + 6);
-router.get('/test', (req, res) => { 
+router.get('/test', (req, res) => {
     let a
-    if(1==null){a = 5}else{a=2}
+    if (1 == null) { a = 5 } else { a = 2 }
     res.json(a);
 });
 router.get('/profile', (req, res) => { res.json(req.user) });

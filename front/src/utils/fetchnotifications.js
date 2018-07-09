d340240a15ae5121ece813c11e2132e8f52e8794
Erno <erno.viitanen1@student.lamk.fi>
2018-07-09 11:18:05 +0300

    ylimääräiset pois

--- front/src/utils/fetchnotifications.js
+++ front/src/utils/fetchnotifications.js
@@ -2,23 +2,10 @@ import axios from 'axios';
 import { BASE_URL } from '../settings';
 
 
-export { getNotifications, addNotification, editNotification };
+export { getNotifications, editNotification };
 
 function getNotifications() {
   return axios.get(BASE_URL + '/announcement').then(response => response.data);
-}
-
-function addNotification(notificationData) {
-  return axios.post(BASE_URL + '/announcementAdd', {
-    title: notificationData.title,
-    info: notificationData.info,
-    dateBegin: notificationData.dateBegin,
-    dateEnd: notificationData.dateEnd
-  })
-    .then(response => response.data)
-    .catch(function (error) {
-      return error;
-    });
 }
 
 function editNotification(metodi, notificationData) {
